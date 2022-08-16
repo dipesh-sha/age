be390493c5b90a3bbbea036e76694990aeb19572
dipesh-sha <111327853+dipesh-sha@users.noreply.github.com>
2022-08-16 08:04:47 +0545

    Add files via upload

---
+++ elseif.py
@@ -0,0 +1,11 @@
+print("Enter Your age")
+age = int(input())
+
+if age < 18 :
+    print("Sorry, You can't Drive")
+elif age == 18:
+    print("We will think about this")
+elif age <= 100:
+    print("You can drive")
+else :
+    print("Enter the age greater than 7 and less than 100")
